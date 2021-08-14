bd5a225ed3a08d0887699857f9137f4dc6698bbf
nurbrun <the.nurbrun@gmail.com>
2021-08-13 20:37:07 -0400

    deploying to gh pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import ChartArea from './components/ChartArea';
+import { HashRouter as Router } from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <div className="App">
-    <ChartArea />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+            <Route exact path="/">
+                <div className="App">
+                    <ChartArea />
+                </div>
+            </Route>
+        </Switch>
+    </Router>
     </>
   );
 }
